4bdea1cac274f4ed4fd5c02b592bd0d9c6f8128d
xanderyanov <xanderyanov@gmail.com>
2021-06-15 19:10:30 +0400

    19-10

---
+++ src/assets/js/__leftSlideMenu.js
@@ -0,0 +1,26 @@
+/*start-newSlideMobileMenu*/
+
+$(".leftSlideMenu_js").on("click", function (e) {
+	e.preventDefault();
+	$(".leftSlideMenu__overlay").fadeIn();
+	$(".leftSlideMenu__wrapper").addClass("leftSlideMenu__wrapper_open");
+	$("body").addClass("stop");
+});
+
+$(".leftSlideMenu__close").on("click", function (e) {
+	e.preventDefault();
+	$(".leftSlideMenu__wrapper").removeClass("leftSlideMenu__wrapper_open");
+	$(".leftSlideMenu__overlay").fadeOut();
+	$("body").removeClass("stop");
+});
+$(".leftSlideMenu__wrapper").on("click", function (e) {
+	e.stopPropagation();
+});
+$(".leftSlideMenu__overlay").on("click", function (e) {
+	e.preventDefault();
+	$(".leftSlideMenu__wrapper").removeClass("leftSlideMenu__wrapper_open");
+	$(".leftSlideMenu__overlay").fadeOut();
+	$("body").removeClass("stop");
+});
+
+/*end-newSlideMobileMenu*/
