5f0a4545dbfddc428abfd75486e537539b930dcb
xanderyanov <xanderyanov@gmail.com>
2021-07-26 17:18:48 +0400

    17-18

--- src/assets/js/__eShopCabinet.js
+++ src/assets/js/__eShopCabinet.js
@@ -1,34 +1,34 @@
 $(function () {
-  if ($window.width() <= 768) {
-    $(".cab2__tab > a").removeClass("active");
-    $(".cab2__tab > section").removeClass("openSection").slideUp();
-    $(".cab2__tabs").children("li").first().children("a").removeClass("active").next().removeClass("open").hide();
-  } else {
-    $(".cab2__tabs").children("li").first().children("a").addClass("active").next().addClass("open").show();
-  }
+	if ($window.width() <= 768) {
+		$(".cab2__tab > a").removeClass("active");
+		$(".cab2__tab > section").removeClass("openSection").slideUp();
+		$(".cab2__tabs").children("li").first().children("a").removeClass("active").next().removeClass("open").hide();
+	} else {
+		$(".cab2__tabs").children("li").first().children("a").addClass("active").next().addClass("open").show();
+	}
 
-  // function eShopCabTabDesktop() {
-  $(".cab2__tabs").on("click", "li > a", function (e) {
-    e.preventDefault();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-      if ($window.width() <= 768) {
-        $(this).next().removeClass("openSection").slideUp();
-      } else {
-        $(this).next().removeClass("openSection").hide();
-      }
-    } else {
-      $(".cab2__tab > a").removeClass("active");
-      $(this).addClass("active");
-      if ($window.width() <= 768) {
-        $(".cab2__tab > section").removeClass("openSection").slideUp();
-        $(this).next().addClass("openSection").slideDown();
-      } else {
-        $(".cab2__tab > section").removeClass("openSection").hide();
-        $(this).next().addClass("openSection").show();
-      }
-    }
-  });
-  // }
-  // eShopCabTabDesktop();
+	// function eShopCabTabDesktop() {
+	$(".cab2__tabs").on("click", "li > a", function (e) {
+		e.preventDefault();
+		if ($(this).hasClass("active")) {
+			if ($window.width() <= 768) {
+				$(this).next().removeClass("openSection").slideUp();
+				$(this).removeClass("active");
+			} else {
+				// $(this).next().removeClass("openSection").hide();
+			}
+		} else {
+			$(".cab2__tab > a").removeClass("active");
+			$(this).addClass("active");
+			if ($window.width() <= 768) {
+				$(".cab2__tab > section").removeClass("openSection").slideUp();
+				$(this).next().addClass("openSection").slideDown();
+			} else {
+				$(".cab2__tab > section").removeClass("openSection").hide();
+				$(this).next().addClass("openSection").show();
+			}
+		}
+	});
+	// }
+	// eShopCabTabDesktop();
 });
